33763e8a6f55e20b552fd39d4d5ffaf2aad55101
sol1230 <solllll1230@gmail.com>
2022-10-18 12:52:48 +0900

    CategoryName

--- Lee_Eunsol/w3schools/CategoryName_Products.sql
+++ Lee_Eunsol/w3schools/CategoryName_Products.sql
@@ -1,17 +1,8 @@
--- CategoryName이 Dairy Products, Seafood이고 Price가 10이상 50이하인 합계는?
-SELECT SUM(Price)
+-- CategoryName이 Dairy Products, Seafood이고 Price가 10이상 50이하인 합계와 평균은?
+SELECT SUM(Price), AVG(Price)
 FROM Products
 WHERE CategoryID IN (SELECT CategoryID 
 					FROM Categories
 					WHERE CategoryName IN('Dairy Products', 'Seafood'))
 AND Price BETWEEN 10 AND 50;
-    SUM(Price) 390.34
-
--- CategoryName이 Dairy Products, Seafood이고 Price가 10이상 50이하인 평균은?
-SELECT AVG(Price)
-FROM Products
-WHERE CategoryID IN (SELECT CategoryID 
-					FROM Categories
-					WHERE CategoryName IN('Dairy Products', 'Seafood'))
-AND Price BETWEEN 10 AND 50;
-    AVG(Price) 24.396250
+    SUM(Price) 390.34, AVG(Price) 24.396250
