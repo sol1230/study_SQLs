ace14727da7368bfad238a5ce8a70190a2840204
sol1230 <111327647+sol1230@users.noreply.github.com>
2022-10-18 13:20:48 +0900

    Update CategoryName_Products_Order.sql

--- Lee_Eunsol/w3schools/CategoryName_Products_Order.sql
+++ Lee_Eunsol/w3schools/CategoryName_Products_Order.sql
@@ -2,11 +2,11 @@
 SELECT OrderID
 FROM Orders
 WHERE OrderID IN (SELECT OrderID
-					FROM OrderDetails
-					WHERE ProductID IN (SELECT ProductID
-										FROM Products
-										WHERE Price BETWEEN 10.00 AND 50.00
-                    AND CategoryID IN (SELECT CategoryID
-										FROM Categories
-										WHERE CategoryName IN('Dairy Products', 'Seafood'))));
+			FROM OrderDetails
+			WHERE ProductID IN (SELECT ProductID
+						FROM Products
+						WHERE Price BETWEEN 10.00 AND 50.00
+			      AND CategoryID IN (SELECT CategoryID
+						FROM Categories
+						WHERE CategoryName IN('Dairy Products', 'Seafood'))));
     Number of Records: 379, OrderID 10248~11077
