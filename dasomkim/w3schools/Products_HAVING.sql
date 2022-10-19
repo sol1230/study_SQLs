31e3f79c1a05df39b443090a9a82702e72286eab
mayds1010 <mayds1010@gmail.com>
2022-10-19 10:25:16 +0900

    Having

--- dasomkim/w3schools/Products_HAVING.sql
+++ dasomkim/w3schools/Products_HAVING.sql
@@ -1,13 +1,12 @@
 --카테고리에 제품이 10개 이상인 카테고리가 무엇인지 확인하시오
-SELECT CategoryID,COUNT(ProductID)
+SELECT COUNT(ProductID),CategoryID
 FROM Products
 GROUP BY CategoryID
 HAVING COUNT(CategoryID)>= 10;
---CategoryID	COUNT(CategoryID)
-     1	                12
-     2	                12
-     3	                13
-     4	                10
-     8	                12              
-
+--COUNT(ProductID)	CategoryID
+        12	            1
+        12	            2
+        13	            3
+        10	            4
+        12	            8
  
