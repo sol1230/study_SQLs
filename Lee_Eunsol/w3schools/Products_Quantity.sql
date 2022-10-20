accd8d75e590ef68d2bb1eb9b3375ff779f11049
sol1230 <solllll1230@gmail.com>
2022-10-20 15:15:32 +0900

    Users_Auth_names

--- Lee_Eunsol/w3schools/Products_Quantity.sql
+++ Lee_Eunsol/w3schools/Products_Quantity.sql
@@ -3,3 +3,7 @@ SELECT Products.ProductName, Products.Price, OrderDetails.Quantity
 FROM Products INNER JOIN OrderDetails
 ON Products.ProductID = OrderDetails.ProductID;
     Number of Records: 2155
+
+-- 주문자 이름, 주문 날짜, 주문 갯수
+
+-- 제품명, 공급자명, 공급자
