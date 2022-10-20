13c37f3f0e75a89474bbd951eaed57f560a01ba6
mayds1010 <mayds1010@gmail.com>
2022-10-20 15:13:46 +0900

    Users_Auth_names

---
+++ dasomkim/w3schools/Users_Auth_names.sql
@@ -0,0 +1,13 @@
+-- 이름, 이메일, 권한이름 표시 / 조건 이름이 'Teddy', 'Allen' ?
+
+SELECT users.NAME,users.EMAIL,auth_names.AUTH_NAME
+FROM (users INNER JOIN auths
+ON users.UNIQUE_ID = auths.UNIQUE_ID_USERS
+AND users.NAME IN ('Teddy', 'Allen'))
+INNER JOIN auth_names 
+ON auth_names.UNIQUE_ID = auths.UNIQUE_ID_AUTH_NAMES
+; 
+-- Allen	texas@imfblog.org	GUEST
+--  Allen	texas@imfblog.org	ADMIN
+--  Allen	texas@imfblog.org	MANAGER
+--  Teddy	norway@iotm.com	    GUEST
